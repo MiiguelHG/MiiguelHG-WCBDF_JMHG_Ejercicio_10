a0fda45d22c9766399a63bbc69849a544b6f2ff9
Jesus Miguel Hernandez Garcia <129304627+MiiguelHG@users.noreply.github.com>
2024-12-19 07:24:56 -0600

    updateCors

--- src/main/java/com/upiiz/superheroes/controllers/HeroeController.java
+++ src/main/java/com/upiiz/superheroes/controllers/HeroeController.java
@@ -15,7 +15,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping("/api/v1/heroes")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class HeroeController {
     @Autowired
     private HeroeService heroeService;
